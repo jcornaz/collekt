d05ea73d2d4637c97a78a573e5e606f907c502ec
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-05-28 22:43:14 +0200

    Add comments

--- common/src/main/kotlin/com/github/jcornaz/collekt/ImmutableCollections.kt
+++ common/src/main/kotlin/com/github/jcornaz/collekt/ImmutableCollections.kt
@@ -5,19 +5,27 @@ import com.github.jcornaz.collekt.impl.EmptyImmutableSet
 import com.github.jcornaz.collekt.impl.ImmutableListAdapter
 
 /**
- * Return an empty [ImmutableList]
+ * Returns an empty [ImmutableList]
  */
-fun <E> emptyImmutableList(): ImmutableList<E> = EmptyImmutableList
+public fun <E> emptyImmutableList(): ImmutableList<E> = EmptyImmutableList
 
 /**
- * Return an empty [ImmutableSet]
+ * Returns an empty [ImmutableSet]
  */
-fun <E> emptyImmutableSet(): ImmutableSet<E> = EmptyImmutableSet
+public fun <E> emptyImmutableSet(): ImmutableSet<E> = EmptyImmutableSet
 
-fun <E> immutableListOf(vararg values: E): ImmutableList<E> =
+/**
+ * Returns a new immutable list containing the given elements
+ */
+public fun <E> immutableListOf(vararg values: E): ImmutableList<E> =
         if (values.isEmpty()) emptyImmutableList() else ImmutableListAdapter(values.toList())
 
-fun <E> Iterable<E>.toImmutableList(): ImmutableList<E> = when {
+/**
+ * Returns a new immutable list all elements in this iterable.
+ *
+ * May create a defensive copy if necessary
+ */
+public fun <E> Iterable<E>.toImmutableList(): ImmutableList<E> = when {
     this is ImmutableList<E> -> this
     none() -> emptyImmutableList()
     else -> ImmutableListAdapter(toList())
