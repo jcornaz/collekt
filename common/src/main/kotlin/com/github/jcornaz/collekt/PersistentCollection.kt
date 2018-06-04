12b95ec219546dfc24dd3699819cc1aef5606bba
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-06-04 23:08:32 +0200

    Comment

--- common/src/main/kotlin/com/github/jcornaz/collekt/PersistentCollection.kt
+++ common/src/main/kotlin/com/github/jcornaz/collekt/PersistentCollection.kt
@@ -1,54 +1,106 @@
 package com.github.jcornaz.collekt
 
 /**
- * An immutable [Collection].
+ * An effectively immutable collection providing *mutation method* which return new instances instead of modifying the instance.
  *
- * All implementers promise that the collection won't ever change after instance creation
+ * Typically a persistent collection is optimized to share as much data as possible between other instances,
+ * reducing memory usage, and providing good performance of the *mutation methods*
  */
 public interface PersistentCollection<out E> {
-    val size: Int
-    val isEmpty: Boolean
 
-    operator fun contains(element: @UnsafeVariance E): Boolean
+    /** Number of element in the collection */
+    public val size: Int
 
-    operator fun iterator(): Iterator<E>
+    /** True if, and only if the collection is empty */
+    public val isEmpty: Boolean
 
-    operator fun plus(element: @UnsafeVariance E): PersistentCollection<E>
-    operator fun plus(collection: PersistentCollection<@UnsafeVariance E>): PersistentCollection<E>
-    operator fun minus(element: @UnsafeVariance E): PersistentCollection<E>
+    /** True if, and only if the collection contains the given [element] */
+    public operator fun contains(element: @UnsafeVariance E): Boolean
+
+    /** Returns an iterator over the elements of the collection */
+    public operator fun iterator(): Iterator<E>
+
+    /** Returns a new collection containing the same elements plus the given [element] */
+    public operator fun plus(element: @UnsafeVariance E): PersistentCollection<E>
+
+    /** Returns a new collection containing the same elements plus the element of the given [collection] */
+    public operator fun plus(collection: PersistentCollection<@UnsafeVariance E>): PersistentCollection<E>
+
+    /** Returns a new collection containing the same elements except the given [element] */
+    public operator fun minus(element: @UnsafeVariance E): PersistentCollection<E>
 }
 
 /**
- * An immutable [List]
+ * An effectively immutable list providing *mutation method* which return new instances instead of modifying the instance.
  *
- * All implementers promise that the list won't ever change after instance creation
+ * Typically a persistent collection is optimized to share as much data as possible between other instances,
+ * reducing memory usage, and providing good performance of the *mutation methods*
  */
 public interface PersistentList<out E> : PersistentCollection<E> {
-    operator fun get(index: Int): E
 
-    fun indexOf(element: @UnsafeVariance E): Int
-    fun lastIndexOf(element: @UnsafeVariance E): Int
+    /**
+     * Returns the element at the specified [index]
+     *
+     * @throws kotlin.IndexOutOfBoundsException If the index is < 0 or >= [size]
+     */
+    public operator fun get(index: Int): E
 
-    fun subList(fromIndex: Int, toIndex: Int): PersistentList<E>
+    /** Returns the first index of the given [element] or `-1` if not present in the list */
+    public fun indexOf(element: @UnsafeVariance E): Int
 
-    override fun plus(element: @UnsafeVariance E): PersistentList<E> = plus(element, size)
-    fun plus(element: @UnsafeVariance E, index: Int): PersistentList<E>
+    /** Returns the last index of the given [element] or `-1` if not present in the list */
+    public fun lastIndexOf(element: @UnsafeVariance E): Int
 
-    override fun plus(collection: PersistentCollection<@UnsafeVariance E>): PersistentList<E> = plus(collection, size)
-    fun plus(collection: PersistentCollection<@UnsafeVariance E>, index: Int): PersistentList<E>
+    /**
+     * Returns a list which contains the elements of this list which are between [fromIndex] (inclusive) and [toIndex] (exclusive)
+     */
+    public fun subList(fromIndex: Int, toIndex: Int): PersistentList<E>
 
-    override fun minus(element: @UnsafeVariance E): PersistentList<E>
-    fun minusIndex(index: Int): PersistentList<E>
+    /** Returns a new list containing the same elements plus the given [element] appended at the end */
+    public override fun plus(element: @UnsafeVariance E): PersistentList<E> = plus(element, size)
+
+    /** Returns a new list containing the same elements plus the given [element] inserted at the given [index] */
+    public fun plus(element: @UnsafeVariance E, index: Int): PersistentList<E>
+
+    /** Returns a new list containing the same elements plus the given [collection] appended at the end */
+    public override fun plus(collection: PersistentCollection<@UnsafeVariance E>): PersistentList<E> = plus(collection, size)
+
+    /** Returns a new list containing the same elements plus                                                    wr*/
+    public fun plus(collection: PersistentCollection<@UnsafeVariance E>, index: Int): PersistentList<E>
+
+    /**
+     * Returns a new list containing the same elements minus the given [element]
+     *
+     * If there is many occurrence of the [element], then the first one is removed in the result list
+     */
+    public override fun minus(element: @UnsafeVariance E): PersistentList<E>
+
+    /**
+     * Returns a new list containing the same elements except the element at given [index]
+     *
+     * @throws kotlin.IndexOutOfBoundsException if the index is < 0 or >= [size]
+     */
+    public fun minusIndex(index: Int): PersistentList<E>
 }
 
+/**
+ * Returns a [Collection] adapter for this [PersistentCollection]
+ */
 public fun <E> PersistentCollection<E>.asCollection(): Collection<E> = object : AbstractCollection<E>() {
     override val size get() = this@asCollection.size
     override fun iterator() = this@asCollection.iterator()
+    override fun contains(element: E) = element in this@asCollection
 }
 
+/**
+ * Returns a [List] adapter for this [PersistentCollection]
+ */
 public fun <E> PersistentList<E>.asList(): List<E> = object : AbstractList<E>() {
     override val size get() = this@asList.size
+    override fun isEmpty() = this@asList.isEmpty
     override fun get(index: Int) = this@asList[index]
+    override fun contains(element: E) = element in this@asList
+    override fun iterator() = this@asList.iterator()
 }
 
 internal interface PersistentCollectionFactory {
--- common/src/main/kotlin/com/github/jcornaz/collekt/PersistentCollectionOperators.kt
+++ common/src/main/kotlin/com/github/jcornaz/collekt/PersistentCollectionOperators.kt
@@ -1,12 +1,23 @@
 package com.github.jcornaz.collekt
 
+/**
+ * Execute [action] for each element in the collection
+ */
 public inline fun <E> PersistentCollection<E>.forEach(action: (E) -> Unit) {
     for (element in this) action(element)
 }
 
+/**
+ * Returns the first element of the collection (first element return by the iterator in case of un-ordered collection)
+ *
+ * @throws kotlin.NoSuchElementException If the collection is empty
+ */
 public fun <E> PersistentCollection<E>.first(): E =
         if (this is PersistentList<E> && !isEmpty) this[0] else iterator().next()
 
+/**
+ * Returns the first element of the collection or null if the collection is empty
+ */
 public fun <E> PersistentCollection<E>.firstOrNull(): E? {
     return if (this is PersistentList<E>) {
         if (isEmpty) null else this[0]
@@ -15,6 +26,9 @@ public fun <E> PersistentCollection<E>.firstOrNull(): E? {
     }
 }
 
+/**
+ * Join the element of the collection in a string
+ */
 public fun <E> PersistentCollection<E>.joinToString(
         separator: String = "",
         prefix: String = "",
