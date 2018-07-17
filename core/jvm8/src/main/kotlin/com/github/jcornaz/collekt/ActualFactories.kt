158deee060ee3cc6c1e272b3516d2898b925d961
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-07-17 20:07:20 +0200

    Use Paguro as a default implementation for JVM 8

--- core/jvm8/src/main/kotlin/com/github/jcornaz/collekt/ActualFactories.kt
+++ core/jvm8/src/main/kotlin/com/github/jcornaz/collekt/ActualFactories.kt
@@ -3,10 +3,10 @@ package com.github.jcornaz.collekt
 import com.github.jcornaz.collekt.api.PersistentListFactory
 import com.github.jcornaz.collekt.api.PersistentMapFactory
 import com.github.jcornaz.collekt.api.PersistentSetFactory
-import com.github.jcornaz.collekt.paguro.PaguroRRBTree
+import com.github.jcornaz.collekt.paguro.PaguroHashMap
 import com.github.jcornaz.collekt.paguro.PaguroHashSet
-import com.github.jcornaz.collekt.stdlib.StdlibMap
+import com.github.jcornaz.collekt.paguro.PaguroRRBTree
 
 public actual var DefaultListFactory: PersistentListFactory = PaguroRRBTree
 public actual var DefaultSetFactory: PersistentSetFactory = PaguroHashSet
-public actual var DefaultMapFactory: PersistentMapFactory = StdlibMap
+public actual var DefaultMapFactory: PersistentMapFactory = PaguroHashMap
