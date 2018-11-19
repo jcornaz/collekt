2f66555177f64456693c0417ac8c92b3130638ce
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-11-19 21:05:05 +0100

    Remove Dexx implementation (dead code)

--- impl/dexx/src/test/kotlin/com/github/jcornaz/collekt/dexx/DexxVectorTest.kt
+++
@@ -1,8 +0,0 @@
-package com.github.jcornaz.collekt.dexx
-
-import com.github.jcornaz.collekt.api.PersistentListFactory
-import com.github.jcornaz.collekt.test.PersistentListTest
-
-class DexxVectorTest : PersistentListTest() {
-    override val factory: PersistentListFactory get() = DexxVector
-}
